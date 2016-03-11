da9cd5fefbbd4680a5e4c35fd9844ca1b7a2dda1
Vincent Tran <vincenttran23@gmail.com>
2016-03-11 00:00:30 -0500

    got messages to appear

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,22 @@
-// Your JavaScript goes here...
+request = new XMLHttpRequest();
+
+request.open("GET", "http://localhost:8000/data.json", true);
+
+request.onreadystatechange = parse;
+
+request.send(null);
+
+function parse() {
+	if (request.readyState == 4 && request.status == 200) {
+		var messageData = JSON.parse(request.responseText);
+		var messages = document.getElementById("messages");
+
+		for(var i = 0; i < messageData.length; i++) {
+			messages.innerHTML += "<p>" + "<span class='text'>" + messageData[i]["content"] + "</span> " + messageData[i]["username"] + "</p>";
+		}
+
+	}
+	else if (request.readyState == 4 && request.status != 200) {
+		document.getElementById("messages").innerHTML = "fail";
+	}
+};
